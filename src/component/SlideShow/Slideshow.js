064be79800bf17d5fe8cc541f33cdd20fd4b3aeb
palmswill <59519523+palmswill@users.noreply.github.com>
2021-01-19 19:33:54 -0500

    commit

--- src/component/SlideShow/Slideshow.js
+++ src/component/SlideShow/Slideshow.js
@@ -2,33 +2,28 @@ import React, { useContext, useState, useEffect, useRef } from 'react';
 import Loader from 'react-loader-spinner';
 import { MovieSearchContext } from '../../context/MovieContext';
 import slideShowStyle from './SlideShow.module.css';
+import detailedDisplayStyle from '../NominationCardDisplay/DetailedDisplay.module.css';
 
 
 
 const SlideShow = () => {
     const { nomination } = useContext(MovieSearchContext);
-    /// the slide index
     const [slide, setSlide] = useState(1);
     ///to hold timeout function
     const timeoutRef = useRef();
-    ///determine if slider is loading
-    const isLoading=useRef(true);
 
 
 
-    
+    ////onclick event of front poster to scroll to its corresponding detailed nomination card
+    const SlidetoDetail = (index) => {
+        document.getElementsByClassName(detailedDisplayStyle.displaygrid)[index].scrollIntoView({behavior:"smooth",block:"center"});
+    }
 
-   
 
-    
 
-    
-    ///change slide to selected slide, reset all other timer
-    const hanldeManualChangeSlide = (index) => {
+    const hanldeManualChangeSlide = (slideNumber) => {
         window.clearTimeout(timeoutRef.current);
-        setSlide(index)
-
-
+        setSlide(slideNumber)
     }
 
 
@@ -45,30 +40,20 @@ const SlideShow = () => {
         }
 
     }, [slide, nomination.length]);
-    
     ////cleartimeout and restart counting if nomination list change
     useEffect(() => {
-        ///notice a significant amount of loading time when nomination is 1,added loader,can hide after image since image does not disappear
-        if (nomination.length===1){
-             isLoading.current=true;
-        }
         return () => window.clearTimeout(timeoutRef.current);
 
 
     }, [nomination.length])
 
-    // console.log(slide)
-
-
-    useEffect(()=>{
-
-    })
+    // console.log(slide);
 
 
     return nomination.length ? (
         <div className={slideShowStyle.slideShowContainer}>
             <div className={slideShowStyle.slideSlot}>
-                { <div className={slideShowStyle.Loader}  style={{display:isLoading.current?("block"):("none")}}>
+                <div className={slideShowStyle.Loader}>
                     <Loader
                         id="loader"
                         type="Grid"
@@ -77,17 +62,17 @@ const SlideShow = () => {
                         width={50}
                     />
 
-                </div> }
+                </div>
                 {nomination.map((value, index) => {
                     return (
                         (index + 1 === slide) ? (
-                            <div key={index + 1}  onLoad={()=>isLoading.current=false} className={`${slideShowStyle.slide} ${slideShowStyle.show}`}>
-                                <div className={slideShowStyle.numberText}>{index + 1 + "/5"} </div>
-                                <img className={slideShowStyle.poster} src={value.poster} alt={value.title} />
-                                <div className={slideShowStyle.text}>{value.title}</div>
+                            <div key={index + 1} onClick={() => SlidetoDetail(index)} className={`${slideShowStyle.slide} ${slideShowStyle.show}`}>
+                                <div className={slideShowStyle.numberText} >{index + 1 + "/5"} </div>
+                                <img className={slideShowStyle.poster} onClick={() => SlidetoDetail(index)} src={value.poster} alt={value.title} />
+                                <div className={slideShowStyle.text} onClick={() => SlidetoDetail(index)}>{value.title}</div>
                             </div>
                         ) : (
-                                <div key={index + 1}  onLoad={()=>isLoading.current=false}  className={slideShowStyle.slide}>
+                                <div key={index + 1} className={slideShowStyle.slide}>
                                     <div className={slideShowStyle.numberText}>{index + 1 + "/5"} </div>
                                     <img className={slideShowStyle.poster} src={value.poster} alt={value.title} />
                                     <div className={slideShowStyle.text}>{value.title}</div>
